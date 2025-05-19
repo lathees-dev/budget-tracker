abcab812cb36032b55e0d43405af854f06232562
Lathees <latheesraja655@gmail.com>
2025-05-19 11:05:13 +0530

    initial commit

---
+++ backend/models/UserModel.js
@@ -0,0 +1,22 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Name required"],
+    },
+    email: {
+      type: String,
+      required: [true, "Email is required"],
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: [true, "Password is required"],
+    },
+  },
+  { timestamp: true }
+);
+
+module.exports = mongoose.model("User", userSchema);
